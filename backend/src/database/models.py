4fb4e9d930e018b4d327f4dd0f980a9e051e64b4
mohameda97 <zagger.com@gmail.com>
2020-08-28 19:54:39 +0200

    modified

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -56,7 +56,7 @@ class Drink(db.Model):
     '''
 
     def short(self):
-       # print(json.loads(self.recipe))
+        print(json.loads(self.recipe))
         short_recipe = [{'color': r['color'], 'parts': r['parts']} for r in json.loads(self.recipe)]
         return {
             'id': self.id,
